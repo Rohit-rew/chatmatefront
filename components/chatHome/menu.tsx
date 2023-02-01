e41499793284b40e893e321fd9e271f37581afcd
rohit <rohit.rew@gmail.com>
2023-02-02 03:27:36 +0530

    user can logout

--- components/chatHome/menu.tsx
+++ components/chatHome/menu.tsx
@@ -7,10 +7,24 @@ import {
   faListDots,
   faClose,
 } from "@fortawesome/free-solid-svg-icons";
+import { useCookies } from "react-cookie";
+import Router from "next/router";
 
 export default function Menu() {
 
   const [isMenueOpen, setIsMenueOpen] = React.useState<boolean>(false);
+
+  const [cookies , setCookies] = useCookies(["chatmate"])
+
+  const handleClick = ()=>{
+    // detete the existing cookies and send to home route
+    setCookies("chatmate" , "" ,{
+      path: "/",
+      sameSite: true,
+      maxAge: 1,
+    })
+    Router.push("/")
+  }
 
   return (
     <div className="flex items-center relative">
@@ -21,7 +35,9 @@ export default function Menu() {
           />
           {isMenueOpen && (
             <div className="absolute bg-white top-10 right-0 shadow-xl border border rounded select-none">
-              <button className="p-2 px-2 hover:bg-gray-200 w-40">
+              <button 
+                onClick={handleClick}
+                className="p-2 px-2 hover:bg-gray-200 w-40">
                 Logout
               </button>
               <button className="p-2 px-2 hover:bg-gray-200 w-40">
